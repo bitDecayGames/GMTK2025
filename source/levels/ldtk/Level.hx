20c46423f78fb05f012725e65aca8400b5a870be
Logan <monday.hopscotch@gmail.com>
2025-07-30 13:34:54 -0600

    multi world

--- source/levels/ldtk/Level.hx
+++ source/levels/ldtk/Level.hx
@@ -16,10 +16,18 @@ class Level {
 	public static var project = new LdtkProject();
 
 	/**
+	 * World id that this level was loaded from
+	**/
+	public var worldID:String;
+
+	/**
 	 * The raw level from the project. Available to get any needed
 	 * one-off values out of the level for special use-cases
 	**/
 	public var raw:Ldtk.Ldtk_Level;
+
+	// This is the painted image for the level itself
+	public var terrainRender:FlxSprite;
 
 	public var terrainLayer:BDTilemap;
 	public var spawnPoint:FlxPoint = FlxPoint.get();
@@ -27,19 +35,21 @@ class Level {
 	public var camZones:Map<String, FlxRect>;
 	public var camTransitions:Array<CameraTransition>;
 
-	public function new(nameOrIID:String) {
-		raw = project.getLevel(nameOrIID);
+	public function new(worldNameOrIID:String, nameOrIID:String) {
+		this.worldID = worldNameOrIID;
+		var world = project.getWorld(worldNameOrIID);
+		raw = world.getLevel(nameOrIID);
 		terrainLayer = new BDTilemap();
 		terrainLayer.loadLdtk(raw.l_Terrain);
 
 		if (raw.l_Objects.all_Spawn.length == 0) {
+			// TODO: probably don't want to throw here as spawns are not the same in this game
 			throw('no spawn found in level ${nameOrIID}');
 		}
-
 		var sp = raw.l_Objects.all_Spawn[0];
 		spawnPoint.set(sp.pixelX, sp.pixelY);
 
-		var test:Ldtk.Entity_Spawn = null;
+		// raw.identifier
 
 		parseCameraZones(raw.l_Objects.all_CameraZone);
 		parseCameraTransitions(raw.l_Objects.all_CameraTransition);
