20654837865a1487176fef8637a696898cbb2ea0
Michael Wingfield <mwingfie@gmail.com>
2025-07-30 16:11:46 -0600

    sorta like a flipper

--- source/entities/Flipper.hx
+++ source/entities/Flipper.hx
@@ -1,14 +1,15 @@
 package entities;
 
+import flixel.FlxG;
 import flixel.FlxSprite;
 import input.SimpleController;
 
 using echo.FlxEcho;
 
 class Flipper extends FlxSprite {
-	var speed:Float = 150;
-	var restingAngle:Float = 150;
-	var flipAngle:Float = 150;
+	var speed:Float = 700;
+	var restingAngle:Float = -30;
+	var flipAngle:Float = 30;
 	var power:Float = 150;
 	var triggerButton:Button;
 
@@ -24,21 +25,17 @@ class Flipper extends FlxSprite {
 		} else {
 			dir = 1 * speed;
 		}
-
-		var xOffset = 0.0;
-		var yOffset = -height * .25;
-
 		this.body = this.add_body({
 			x: x,
 			y: y,
-			kinematic: false,
+			kinematic: true,
 			rotation: restingAngle,
 			shape: {
 				type: RECT,
 				width: 100,
 				height: 100 / 2,
-				offset_x: xOffset,
-				offset_y: yOffset,
+				offset_x: -100,
+				offset_y: 0,
 			}
 		});
 		body.on_move = (x, y) -> this.setPosition(x, y);
@@ -47,23 +44,43 @@ class Flipper extends FlxSprite {
 
 	override public function update(delta:Float) {
 		super.update(delta);
-		// if (SimpleController.just_pressed(triggerButton, 1)) {
-		flip(delta);
-		// }
+		if (FlxG.keys.pressed.SPACE) {
+			flip(delta);
+		} else {
+			rest(delta);
+		}
 	}
 
 	public function flip(delta:Float) {
-		if (dir < 0) {
-			if (body.rotation + dir < flipAngle) {
+		var d = delta * dir;
+		if (d < 0) {
+			if (body.rotation + d < flipAngle) {
 				body.rotation = flipAngle;
 			} else {
-				body.rotation += dir;
+				body.rotation += d;
 			}
 		} else {
-			if (body.rotation + dir > flipAngle) {
+			if (body.rotation + d > flipAngle) {
 				body.rotation = flipAngle;
 			} else {
-				body.rotation += dir;
+				body.rotation += d;
+			}
+		}
+	}
+
+	public function rest(delta:Float) {
+		var d = delta * dir;
+		if (-d < 0) {
+			if (body.rotation - d < restingAngle) {
+				body.rotation = restingAngle;
+			} else {
+				body.rotation -= d;
+			}
+		} else {
+			if (body.rotation - d > restingAngle) {
+				body.rotation = restingAngle;
+			} else {
+				body.rotation -= d;
 			}
 		}
 	}
