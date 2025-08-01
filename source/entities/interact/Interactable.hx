d1c58ad862207507d567cfb5e6f4738c115bed7b
Michael Wingfield <mwingfie@gmail.com>
2025-08-01 14:38:58 -0600

    add target logic

--- source/entities/interact/Interactable.hx
+++ source/entities/interact/Interactable.hx
@@ -1,23 +1,47 @@
 package entities.interact;
 
+import flixel.util.FlxSignal;
 import nape.callbacks.InteractionCallback;
-import bitdecay.flixel.graphics.Aseprite;
-import bitdecay.flixel.graphics.AsepriteMacros;
-import nape.dynamics.InteractionFilter;
-import constants.CGroups;
-import nape.geom.Vec2;
-import nape.shape.Circle;
-import flixel.addons.nape.FlxNapeSprite;
-import nape.phys.Body;
-import nape.phys.BodyType;
 
-class Interactable extends SelfAssigningFlxNapeSprite {
+class Interactable extends SelfAssigningFlxNapeSprite implements Triggerable {
 	private static var degToRad = Math.PI / 180.0;
 	private static var radToDeg = 180.0 / Math.PI;
 
 	public var IID:String;
 
+	private var on:Bool;
+
+	public var disabled:Bool;
+	public var onOffSignal:FlxTypedSignal<Bool->Void> = new FlxTypedSignal<Bool->Void>();
+
+	public function setOn(value:Bool) {
+		if (disabled)
+			return;
+		var different = on != value;
+		on = value;
+		if (different) {
+			onOffSignal.dispatch(on);
+		}
+	}
+
+	public function isOn():Bool {
+		return on;
+	}
+
+	public function resetOnOff() {
+		setOn(false);
+	}
+
 	public function handleInteraction(data:InteractionCallback) {
 		// Override to provide functionality
 	}
+}
+
+interface Triggerable {
+	private var on:Bool;
+	public var disabled:Bool;
+	public var onOffSignal:FlxTypedSignal<Bool->Void>;
+	public function isOn():Bool;
+	public function setOn(value:Bool):Void;
+	public function resetOnOff():Void;
 }
