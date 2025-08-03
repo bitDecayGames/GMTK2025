3f49fe7f2b2c3d15cd90d06178c9c30bc6e319c8
Tristan Havelick <tristan@havelick.com>
2025-08-02 21:12:35 -0600

    make the ball timeout if it hasn't collided with anything for 8 seconds

--- source/states/PlayState.hx
+++ source/states/PlayState.hx
@@ -151,6 +151,7 @@ class PlayState extends FlxTransitionableState {
 		player = new Player(level.spawnPoint.x, level.spawnPoint.y);
 		// player.body.mass = level.rawLevels[0].f_BallMass;
 		player.body.mass = ballMass;
+		player.onTimeout = respawnToTunnel;
 		camera.follow(player, LOCKON, 0.1);
 		playerGroup.add(player.disappearer);
 		playerGroup.add(player.emitter);
